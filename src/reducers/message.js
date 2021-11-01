c3a2da3aa377e084092c0fdbf336403b6ef3ea2d
SandraRusu17 <sandra.rusu17@gmail.com>
2021-11-01 16:33:19 +0200

    Refactor code, removed imports, cleaned code

--- src/reducers/message.js
+++ src/reducers/message.js
@@ -1,16 +1,16 @@
-import { SET_MESSAGE, CLEAR_MESSAGE } from "../actions/types";
+import {SET_MESSAGE, CLEAR_MESSAGE} from "../actions/types";
 
 const initialState = {};
 
 export default function (state = initialState, action) {
-    const { type, payload } = action;
+    const {type, payload} = action;
 
     switch (type) {
         case SET_MESSAGE:
-            return { message: payload };
+            return {message: payload};
 
         case CLEAR_MESSAGE:
-            return { message: "" };
+            return {message: ""};
 
         default:
             return state;

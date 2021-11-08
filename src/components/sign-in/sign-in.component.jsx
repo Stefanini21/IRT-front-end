a4a9b5d4f4ca4df3b8256b9079da0588ee733041
SandraRusu17 <sandra.rusu17@gmail.com>
2021-11-08 20:58:47 +0200

    Login validation

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,99 +1,115 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { authUser } from "../../redux/actions/auth";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {authUser} from "../../redux/actions/auth";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
-import { useHistory } from "react-router-dom";
-import { getUserLoaded } from "../../redux/selectors/auth";
+import {useHistory} from "react-router-dom";
+import {getUserLoaded} from "../../redux/selectors/auth";
 
 const SignIn = () => {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const currentUserLoaded = useSelector(getUserLoaded);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const currentUserLoaded = useSelector(getUserLoaded);
 
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [successful, setSuccessful] = useState(false);
 
-  const required = (value) => {
-    if (!value) {
-      return (
-        <div className="alert alert-danger" role="alert">
-          This field is required!
-        </div>
-      );
-    }
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formattedData = {
-      email: login,
-      password: password,
+    const required = (value) => {
+        if (!value) {
+            return (
+                <div className="alert alert-danger" role="alert">
+                    This field is required!
+                </div>
+            );
+        }
     };
 
-    dispatch(authUser(formattedData));
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-  useEffect(() => {
-    console.log("currentUserLoaded: " + currentUserLoaded);
-    currentUserLoaded ? history.push("/home") : history.push("/login")
-  }, [currentUserLoaded]);
+        setMessage("")
+        setSuccessful(false)
 
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
+        const formattedData = {
+            email: login,
+            password: password,
+        };
 
-        <Form
-          onSubmit={handleSubmit}
+        dispatch(
+            authUser(formattedData)
+        )
+            .then(() => {
+                currentUserLoaded ? history.push("/home") : setMessage('Email or password are incorect')
+            });
 
-          // ref={(c) => {
-          //     this.form = c;
-          // }}
-        >
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Input
-              type="text"
-              className="form-control"
-              name="email"
-              value={login}
-              onChange={(e) => setLogin(e.target.value)}
-              validations={[required]}
-            />
-          </div>
+    }
 
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Input
-              type="password"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              validations={[required]}
-            />
-          </div>
-          <div className="form-group">
-            <button
-              className="btn btn-primary btn-block"
-              // disabled={this.state.loading}
-            >
-              {/*{this.state.loading && (*/}
-              {/*    <span className="spinner-border spinner-border-sm"></span>*/}
-              {/*)}*/}
-              <span>Login</span>
-            </button>
-          </div>
-        </Form>
-      </div>
-    </div>
-  );
-};
 
-export default SignIn;
+    useEffect(() => {
+        console.log("currentUserLoaded: " + currentUserLoaded);
+        currentUserLoaded ? history.push("/home") : history.push("/login")
+    }, [currentUserLoaded]);
+
+    return (
+        <div className="col-md-12">
+            <div className="card card-container">
+                <img
+                    src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                    alt="profile-img"
+                    className="profile-img-card"
+                />
+
+                <Form onSubmit={handleSubmit}>
+
+                    {!successful && (
+                        <div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="email"
+                                    value={login}
+                                    onChange={(e) => setLogin(e.target.value)}
+                                    validations={[required]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <Input
+                                    type="password"
+                                    className="form-control"
+                                    name="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    validations={[required]}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <button className="btn btn-primary btn-block">
+                                    <span>Login</span>
+                                </button>
+                            </div>
+                        </div>
+                    )}
+
+                    {message && (
+                        <div className="form-group">
+                            <div className={successful ? "alert alert-success" : "alert alert-danger"}
+                                 role="alert">
+                                {message}
+                            </div>
+                        </div>
+                    )}
+
+                </Form>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn

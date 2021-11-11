e89a8d9d0d56be782d5bcba9be5b55a85cc9f0e4
SandraRusu17 <sandra.rusu17@gmail.com>
2021-11-11 18:26:16 +0200

    Cleaned code, removed packages

--- src/redux/actions/ticket.js
+++ src/redux/actions/ticket.js
@@ -1,58 +1,54 @@
 import TicketService from "../../services/ticket.service";
-import {
-  CREATE_TICKET_FAIL,
-  CREATE_TICKET_SUCCESS,
-  SET_MESSAGE
-} from "./types";
+import {CREATE_TICKET_FAIL, CREATE_TICKET_SUCCESS, SET_MESSAGE} from "./types";
 
 
 export const createTicket =
-  (title, description, priority, specialty, status, developer) =>
-  (dispatch) => {
-    return TicketService.createTicket(
-      title,
-      description,
-      priority,
-      specialty,
-      status,
-      developer
-    ).then(
-      (response) => {
-        dispatch({
-          type: CREATE_TICKET_SUCCESS,
-          payload: response.data,
-        });
+    (title, description, priority, specialty, status, developer) =>
+        (dispatch) => {
+            return TicketService.createTicket(
+                title,
+                description,
+                priority,
+                specialty,
+                status,
+                developer
+            ).then(
+                (response) => {
+                    dispatch({
+                        type: CREATE_TICKET_SUCCESS,
+                        payload: response.data,
+                    });
 
-        dispatch({
-          type: SET_MESSAGE,
-          payload: response.data.message,
-        });
+                    dispatch({
+                        type: SET_MESSAGE,
+                        payload: response.data.message,
+                    });
 
-        return Promise.resolve();
-      },
-      (error) => {
-        const message =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
+                    return Promise.resolve();
+                },
+                (error) => {
+                    const message =
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString();
 
-        dispatch({
-          type: CREATE_TICKET_FAIL,
-        });
+                    dispatch({
+                        type: CREATE_TICKET_FAIL,
+                    });
 
-        dispatch({
-          type: SET_MESSAGE,
-          payload: message,
-        });
+                    dispatch({
+                        type: SET_MESSAGE,
+                        payload: message,
+                    });
 
-        return Promise.reject();
-      }
-    );
-  };
+                    return Promise.reject();
+                }
+            );
+        };
 
-  export const getAllUsersBySpecialty = (specialty) => (dispatch) => {
+export const getAllUsersBySpecialty = (specialty) => (dispatch) => {
     const url = routes.BASIC_URL + routes.BASIC_PATH + routes.USER_BY_ID + userId;
     console.log(userId + " this is userid")
     console.log(url + " urlllll")
@@ -64,4 +60,4 @@ export const createTicket =
                 payload: response
             })
         })
-      }
+}
--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,13 +1,13 @@
 import {store} from "../store";
 
 export default function authHeader() {
-  const state = store.getState();
-  const { userData } = state.auth;
-  const token = userData?.accessToken;
+    const state = store.getState();
+    const {userData} = state.auth;
+    const token = userData?.accessToken;
 
-  if (token) {
-     return { Authorization: 'Bearer ' + userData.accessToken };
-  } else {
-    return {};
-  }
+    if (token) {
+        return {Authorization: 'Bearer ' + userData.accessToken};
+    } else {
+        return {};
+    }
 }

cdce6716502505eac148b5640e944196ca7c07b6
SandraRusu17 <sandra.rusu17@gmail.com>
2021-11-18 22:56:55 +0200

    Refactor footer and homepage

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,23 +1,45 @@
 import React from "react";
+import {FaFacebook, FaInstagram, FaSlack, FaTwitter, FaYoutube} from 'react-icons/fa'
+import {Col, Container, Row} from "react-bootstrap";
+
 
 const footerStyle = {
-    'background-color': 'green',
-    'border-top': '2px solid red',
-    'position': 'fixed',
+    'margin-top': '235px',
+    'padding': '1rem',
+    'bottom': '0',
+    'left': '0',
     'width': '100%',
-    'bottom': 0,
-    'color': 'white',
-    'font-size': '25px',
+    'textAlign': 'center',
+    'font-size': '20px',
     'background-image': 'linear-gradient(to bottom right, #3b6a9a, #80a9d1)'
 
 }
 
 const Footer = () => {
     return (
-        <div className="container">
-            <footer className="py-5">
-                <div className="d-flex" style={footerStyle}>
-                    <p>&copy; 2021 IRT powered by Stefanini, Inc. All rights reserved.</p>
+        <div>
+            <footer>
+                <div style={footerStyle}>
+
+                    <Container>
+                        <Row className="justify-content-md-center">
+                            <Col xs lg="2">
+                                <a href="#" style={{color: 'white'}}><FaFacebook/></a>
+                            </Col>
+                            <Col xs lg="2">
+                                <a href="#" style={{color: 'white'}}><FaTwitter/></a>
+                            </Col>
+                            <Col xs lg="2">
+                                <a href="#" style={{color: 'white'}}><FaInstagram/></a>
+                            </Col>
+                            <Col xs lg="2">
+                                <a href="#" style={{color: 'white'}}><FaYoutube/></a>
+                            </Col>
+                            <Col xs lg="2">
+                                <a href="#" style={{color: 'white'}}><FaSlack/></a>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </footer>
         </div>
